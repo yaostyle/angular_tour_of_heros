6681e8ef07ea95cfc9f63071aeba775838a98899
Chris <yaostyle@gmail.com>
2018-10-26 12:50:30 -0700

    Display list of heroes. Show detail when click it. Apply *ngIf and *ngFor.

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,13 +1,19 @@
 import 'package:angular/angular.dart';
 import 'package:angular_forms/angular_forms.dart';
+
 import 'src/hero.dart';
+import 'src/mock_heroes.dart';
 
 @Component(
   selector: 'my-app',
   templateUrl: 'app_component.html',
-  directives: [formDirectives],
+  styleUrls: ['app_component.css'],
+  directives: [coreDirectives, formDirectives],
 )
 class AppComponent {
   final title = 'Tour of Heroes';
-  Hero hero = Hero(1, 'Windstorm');
+  List<Hero> heroes = mockHeroes;
+  Hero selected;
+
+  void onSelect(Hero hero) => selected = hero;
 }
