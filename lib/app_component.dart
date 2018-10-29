3b2f26daa7cc4342b2a199dc221e324671ccd3b0
Chris <yaostyle@gmail.com>
2018-10-29 10:24:43 -0700

    Add routing. (Issues found and no solution for now.)

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -2,15 +2,18 @@ import 'package:angular/angular.dart';
 
 import 'src/hero_service.dart';
 import 'src/hero_list_component.dart';
+import 'src/routes.dart';
 
 @Component(
   selector: 'my-app',
   template: '''
     <h1>{{title}}</h1>
     <my-heroes></my-heroes>  
+    <router-outlet [routes]="Routes.all"></router-outlet>
   ''',
-  directives: [HeroListComponent],
+  directives: [routerDirectives],
   providers: [ClassProvider(HeroService)],
+  exports: [RoutePaths, Routes],
 )
 class AppComponent {
   final title = "Tour of Heroes";
